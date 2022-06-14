899210abf59e4414c0bedc839d187578ab2ba637
Vikas Z <Vikas Z vikas.zingade@skillate.com>
2022-06-14 15:04:51 +0530

    Modify solution to find duplicate number

---
+++ knuckle_crack/algorithms/duplicate_number.py
@@ -0,0 +1,22 @@
+def find_duplicate_number(array: list[int]) -> int | None:
+    """
+    Write a function that finds the duplicate number in an unsorted array containing
+    every number from 1 to 100.
+
+    Only one of the numbers will appear twice.
+
+    :return:
+        Return the duplicate number or None if there is no duplicate number.
+    """
+    expected_array = set(range(min(array), max(array) + 1))
+    return expected_array.difference(array) or None
+
+
+if __name__ == "__main__":
+    # List: 1...100
+    input_array = list(range(1, 101))
+    # Replace 14 with 13. 13 is the duplicate number
+    input_array[13] = 13
+
+    missing_number = find_duplicate_number(input_array)
+    print(f"Missing number: {missing_number}")
